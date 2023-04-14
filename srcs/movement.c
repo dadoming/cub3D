c6cc4c62997c1d33493effd8cde9cd0815f76f2a
Antonio Maria da silva pinto ribeiro da cunha <amaria-d@c2r1s2.42lisboa.com>
2023-04-14 14:51:13 +0100

    Removed superfluous move funcs

--- srcs/movement.c
+++ srcs/movement.c
@@ -11,65 +11,13 @@ int p_move(t_game *game, int ns, int ew)
 
     if (ns + ew < -2 || ns + ew > 2)
         return (0);
-    newpos.x = ppos.x + cos(game->player.theta) * ns;
-    newpos.y = ppos.y + sin(game->player.theta) * ew;
+    newpos.x = ppos.x + cos(game->player.theta) * ns * WALKDIST;
+    newpos.y = ppos.y + sin(game->player.theta) * ew * WALKDIST;
+
+	//TODO: check collisions!
 
     ppos = newpos;
-    // if (fabs(ppos.x) > WALKDIST)
-    // {
-    //     game->player.pos.x += 1; //TODO: protect!
-    //     ppos.x = 0;
-    // }
-    // // else if (fabs(ppos.x) < 0)
-    // if (fabs(ppos.y) < 0)
-    // {
-    //     game->player.pos.y += 1; //TODO: protect!
-    //     ppos.y = 0;
-    // }
+
     printf("(%d,%d)__", (int)game->player.pos.x, (int)game->player.pos.y);
-    printf("(%f,%f)\n", game->player.pos.x, game->player.pos.y);
-    
+    printf("(%f,%f)\n", game->player.pos.x, game->player.pos.y);    
 }
-
-// int p_movefrwd(t_game *game)
-// {
-//     t_vec2f newpos;
-
-//     newpos.x = ppos.x + cos(game->player.theta);
-//     newpos.y = ppos.y + sin(game->player.theta);
-
-//     ppos = newpos;
-//     // if (fabs(ppos.x) > WALKDIST)
-//     // {
-//     //     game->player.pos.x += 1; //TODO: protect!
-//     //     ppos.x = 0;
-//     // }
-//     // // else if (fabs(ppos.x) < 0)
-//     // if (fabs(ppos.y) < 0)
-//     // {
-//     //     game->player.pos.y += 1; //TODO: protect!
-//     //     ppos.y = 0;
-//     // }
-//     printf("(%d,%d)__", (int)game->player.pos.x, (int)game->player.pos.y);
-//     printf("(%f,%f)\n", game->player.pos.x, game->player.pos.y);
-// }
-
-
-
-// int p_movebkwd(t_game *game)
-// {
-//     ppos.x -= cos(game->player.theta);
-//     ppos.y += sin(game->player.theta);
-//     if (fabs(ppos.x) > WALKDIST)
-//     {
-//         game->player.pos.x--; //TODO: protect!
-//         game->player.pposset.x = 0;
-//     }
-//     if (fabs(ppos.y) > WALKDIST)
-//     {
-//         game->player.pos.y--; //TODO: protect!
-//         game->player.pposset.y = 0;
-//     }
-//     printf("(%d,%d)__", game->player.pos.x, game->player.pos.y);
-//     printf("(%f,%f)\n", game->player.pposset.x, game->player.pposset.y);
-// }
