907f2645c46b3e419faba766bc8dd5c73e3da88e
Antonio Maria da silva pinto ribeiro da cunha <amaria-d@c1r17s8.42lisboa.com>
2023-05-09 17:23:09 +0100

    Made first object be any object. Not just door

--- srcs/raycaster/ray_calculation.c
+++ srcs/raycaster/ray_calculation.c
@@ -34,7 +34,7 @@ static void hit_wall(t_raycast *ray, t_game *game)
             ray->side = 1;
         }
         if (ray->x == (WINDOWSIZE_X / 2) && ray->block == 0 && (game->objmap[ray->mapX][ray->mapY]) && \
-            (game->objmap[ray->mapX][ray->mapY]->type == DOOR))
+            (game->objmap[ray->mapX][ray->mapY]->type != WALL))
         {
             ray->block = 1;
             ray->first_obj_X = ray->mapX;
