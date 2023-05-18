7f0c1e4548a2b892aca9578c8519d4b1390ec573
Antonio Maria da silva pinto ribeiro da cunha <amaria-d@c2r2s1.42lisboa.com>
2023-05-18 17:30:04 +0100

    Removed frameDraw limit

--- srcs/run_game.c
+++ srcs/run_game.c
@@ -16,12 +16,12 @@ int	theloop(t_game *game)
 
 
     delta = timestampdiff(now, game->old_time);
-    if (delta > 17000) // 60fps
-    {
+    // if (delta > 5000) // 60fps
+    // {
         mlx_put_image_to_window(game->mlx, game->win, game->imgbuffer.img, 0, 0);
         clock_gettime(CLOCK_MONOTONIC, &game->old_time);
         return (0);
-    }
+    // }
     // printf("NoFrameDrawn\n");
     return (1);
 }
